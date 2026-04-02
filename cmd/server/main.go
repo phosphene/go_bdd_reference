0e200f0c90dc4745ed1d5deb52fb647f0a8c59c7
Ed Phillips <edfaustly@gmail.com>
2026-04-01 18:41:12 -0700

    new mods

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,49 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
 
-	handlers "github.com/phosphene/go_bdd_reference/internal/http"
+	_ "github.com/lib/pq"
+	baseHandlers "github.com/phosphene/go_bdd_reference/internal/http"
+	"github.com/phosphene/go_bdd_reference/internal/user"
 )
 
 func main() {
-	http.HandleFunc("/health", handlers.HealthHandler)
+	// 1. Database connection logic (optional for unit testing environment)
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPass := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
 
+	var userRepo user.Repository
+
+	if dbHost != "" {
+		connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+			dbHost, dbPort, dbUser, dbPass, dbName)
+
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			fmt.Printf("Error opening database: %v\n", err)
+			os.Exit(1)
+		}
+		defer db.Close()
+		userRepo = user.NewPostgresRepository(db)
+	}
+
+	// 2. Initialize Hanlers
+	regHandler := user.NewRegistrationHandler(userRepo)
+
+	// 3. Routing
+	http.HandleFunc("/health", baseHandlers.HealthHandler)
+	if userRepo != nil {
+		http.Handle("/users", regHandler)
+	}
+
+	// 4. Start Server
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
